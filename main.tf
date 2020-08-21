4e0f7b901fe5a1096d2d0ea56a714317eb217837
0ni0nrings <strike3test@hotmail.com>
2020-08-21 09:38:32 +0000

    trying state lock 2

--- main.tf
+++ main.tf
@@ -2,8 +2,22 @@
 # 
 ########################################################
 
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.36.0"
+}
 
 
+# Calling the statelock module/statelock
+module "statelock" {
+  source                      = "./modules/statelock"
+  name_of_s3_bucket           = "anewbucketforterraformstatelock"
+  dynamo_db_table_name        = "anewddbforterraformstatelock"
+  iam_user_name               = "cloud_user"
+  }
+
+/*
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=0.12.13"
@@ -15,19 +29,4 @@ terraform {
    encrypt        = true
   }
 }
-
-# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
-provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.36.0"
-}
-
-/*
-# Calling the statelock module/statelock
-module "statelock" {
-  source                      = "./modules/statelock"
-  name_of_s3_bucket           = var.s3_bucket_name
-  dynamo_db_table_name        = var.dynamo_db_table_name
-  iam_user_name               = var.iam_user_name
-  }
 */
