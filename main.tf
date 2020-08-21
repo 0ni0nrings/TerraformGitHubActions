c588dfc43751e808e9cd2db0ceac88e131146c2c
0ni0nrings <strike3test@hotmail.com>
2020-08-21 19:50:41 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,28 @@ module "statelock" {
   iam_user_name               = var.iam_user_name
   }
 */
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  tags = {
+    Name      = "Vpc"
+    Terraform = "true"
+  }
+}
+# Build route table 1
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable1"
+    Terraform = "true"
+  }
+}
+# Build route table 2
+resource "aws_route_table" "route_table2" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable2"
+    Terraform = "true"
+  }
+}
